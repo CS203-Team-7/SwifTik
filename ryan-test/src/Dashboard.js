26e10caa3102f536ed4a8921e13c2e006e49055b
rmmsia <ryan.sia.2022@scis.smu.edu.sg>
2023-09-12 16:34:55 +0800

    Added basic dashboard features

--- ryan-test/src/Dashboard.js
+++ ryan-test/src/Dashboard.js
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Dashboard = () => {
-    return (
-      <div>
-        <p>Welcome to your Dashboard</p>
-      </div>
-    );
-  };
-  export default Dashboard;
+    const navigate = useNavigate();
+    const handleLogOut = () => {
+        localStorage.setItem("authenticated", false);
+        navigate("/login");
+    }
+    const [authenticated, setauthenticated] = useState(null);
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem("authenticated");
+        if (loggedInUser) {
+            setauthenticated(loggedInUser);
+        }
+    }, []);
+
+    if (!authenticated) {
+        // Redirect
+        navigate('/login');
+    } else {
+        return (
+            <div>
+                <p>Welcome to your Dashboard</p>
+                <img src="https://gifdb.com/images/high/default-dance-gif-file-1273kb-rlovu8944rmxtcdh.gif" />
+                <div>
+                    <button type="button" onClick={handleLogOut}> Log Out</button>
+                </div>
+            </div>
+        );
+    }
+};
+
+export default Dashboard;
